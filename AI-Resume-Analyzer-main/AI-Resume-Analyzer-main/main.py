dfa58390e7645283c8494ceacbfc4927b480aa11
Ankur Bharti <131337893+greybarley@users.noreply.github.com>
2025-08-26 21:54:04 +0530

    Update main.py

--- AI-Resume-Analyzer-main/AI-Resume-Analyzer-main/main.py
+++ AI-Resume-Analyzer-main/AI-Resume-Analyzer-main/main.py
@@ -1,6 +1,6 @@
 import streamlit as st                            # For Web Interface Front-End
 from pdfminer.high_level import extract_text      # To Extract Text from Resume PDF
-from sentence_transformers import SentenceTransformer      # To generate Embeddings of text
+from sentence_transformers import SentenceTransformer     # To generate Embeddings of text
 from sklearn.metrics.pairwise import cosine_similarity    # To get Similarity Score of Resume and Job Description
 from groq import Groq                             # API to use LLM's
 import re                                         # To perform Regular Expression Function
@@ -173,6 +173,7 @@ if st.session_state.form_submitted:
         )
     
     
+
 
 
 
